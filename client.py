42eaa2420930e94deb19821398240d1c62a61864
Laketequin1 <Laketequin1@gmail.com>
2024-06-11 00:38:33 +1200

    Adding sql and eval

--- client.py
+++ client.py
@@ -1,9 +1,66 @@
 import socket
 import ssl
+import ast
 
 # Server settings
 HOST = '127.0.0.1'
 PORT = 65432
+
+show_warnings = True
+
+def warn(message): # Displays warning/error messages
+    if show_warnings == True: # If warning messages active
+        print(f"[WARNING] {message}") # Print warning message
+
+def eval_message(message):
+    """
+    Safely return the eval of a string.
+    
+    The function attempts to safely return the eval of a string with ast.literal_eval. If the string can not be evaled due to a ValueError then call warn and return None.
+    
+    This is useful for converting the string with a dictionary inside to just a dictionary, without causing any security risks.
+
+    Parameters
+    ----------
+    message : string
+        A evaluable string.
+
+    Returns
+    -------
+    any
+        The result of the evaled string.
+        
+    ** ValueError **
+    If string cannot be evaled due to ValueError raised:
+    
+    NoneType
+        None.
+        
+    Examples
+    --------
+    >>> message = eval_message("{'one':1, 'two':2, 'three':3}")
+    >>> print(message)
+    {'one':1, 'two':2, 'three':3}
+
+    ** ValueError **
+    If string cannot be evaled due to ValueError raised:
+    
+    >>> message = eval_message("{'one':1, 'two:2, 'three':3}")
+    [WARNING] Unable to eval message recieved: "{'one':1, 'two:2, 'three':3}"
+    >>> print(message)
+    None
+    """
+    if type(message) == str and message:
+        try:
+            return ast.literal_eval(message)
+        except ValueError:
+            warn(f'Unable to eval message recieved: "{message}"')
+    elif message:
+        warn(f"Message length is 0")
+        return ""
+    else:
+        warn(f"Message type is not str: {type(message)}")
+        return None
 
 # Create a socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,10 +75,13 @@ secure_socket.connect((HOST, PORT))
 
 try:
     # Send data
-    secure_socket.sendall(b'Hello, server')
+    secure_socket.sendall(b'{"WatchID": 29542649, "WatchActive": True}')
 
     # Receive data
-    data = secure_socket.recv(1024).decode('utf-8')
+    raw_data = secure_socket.recv(1024).decode('utf-8')
+
+    data = eval_message(raw_data)
+
     print(f"Received: {data}")
 
 except Exception as e:
